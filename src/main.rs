103a81eddbe37befb595be399ae4f5c5b347557c
samy007 <samy2014@free.fr>
2023-05-29 11:53:26 +0200

    made a few speed tests

--- src/main.rs
+++ src/main.rs
@@ -6,70 +6,32 @@
 
 mod lexer;
 mod parser;
-// mod execute;
+mod execute;
+
 
 use parser::Parser;
-/*use execute::stack_bytecode::{
-	vm::Vm,
-	compiler::compile
-};*/
-
-/* 
-fn main() {
-	
-	// let mut parser = Parser::new(
-	// 	"
-	// 	let total = 0;
-	// 	let count = arg_0;
-	// 	while (count > 0) {
-	// 		total = total + arg_1;
-	// 		count = count - 1;
-	// 	}
-	// "
-	// );
-	let mut parser = Parser::new("let a = 5; print(-a);");
-	for x in parser.parse().unwrap() {
-		println!("{x}");
-	}
-
-	// let blk = parser.parse().unwrap();
-	// let mut locals = HashMap::new();
-	// let args = [10000, 13];
-	// execute::walker::walk(blk, &mut locals, args);
-	// println!("{:?}", locals);
-
-}
-*/
 
 
 fn main() {
-	
-	// let mut parser = Parser::new("
-	// let a = 1;
-	// let b = a + 2 * 3;
-	// {
-	// 	let c = 25;
-	// 	print(a + b + c);
-	// };
-	// print(a * b);
-	// ");
-	let mut parser = Parser::new("
-	// let abcd ( -10
-	// print(abcd;
-	;;;
-	fn abcd(): t {
-		return -5 + 4 * 3
+		let mut parser = Parser::new("
+	let t = clock();
+	let i = 0;
+	while(clock() - t < 1000) {
+		i = i + 1;
 	}
+	print(i);
 ");
 	let parsed = parser.parse();
-	// let res = compile(parsed);
-
-	println!("{parsed:?}");
-/*
-let mut prog = Vm::new(res.0, res.1);
-
-prog.run();
-println!("{prog:?}");
-*/
 	
+	let compiled = execute::stack_bytecode::compiler::compile(parsed.0); // 1.6M
+
+
+	let mut vm = execute::stack_bytecode::vm::Vm::new(compiled.0, compiled.1);
+
+	vm.run();
+
+	// execute::walker::walk(parsed.0, &mut locals, args); // 420k
+	// execute::stack_bytecode::compiler::compile(parsed.0); // 1.680M
+	// js: 18.8M
+	// python: 6M	
 }
