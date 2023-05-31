a9c73fe3244e1661f596b79a42bd9ffb13b73e54
samy007 <samy2014@free.fr>
2023-05-31 11:48:04 +0200

    listed a few syntax ideas

---
+++ lang_ideas.rs
@@ -0,0 +1,46 @@
+pub fn abcd<T>(arg1: T) -> T {
+	arg1 + 2
+}
+
+for () {
+
+}
+
+while () {
+
+} ( finally {} )
+
+
+mod module {
+
+}
+
+
+const abcd = 10;
+
+
+enum abcd {
+	abdc = 5,
+	efgh
+}
+
+
+buildtime {
+
+}
+
+Result<Vec<T>, Option<Vec<E>>>
+T[]!E[]?
+
+
+
+struct abcd {
+	a: number,
+	pub b: number
+}
+
+impl abcd {
+
+}
+
+// maybe traits
