12c8bb1790987658b2a926e3231c0adcc0b90ad1
samy007 <samy2014@free.fr>
2023-06-05 18:22:53 +0200

    move main to run vm tests

---
+++ src/_main.rs
@@ -0,0 +1,96 @@
+// using https://domenicquirl.github.io/blog/parsing-basics/
+// FIXME:
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![allow(clippy::inline_always)]
+// #![warn(clippy::cargo)]
+
+mod execute;
+mod lexer;
+mod parser;
+
+use parser::Parser;
+
+const CODE: &str = "let t: number = clock();
+let i: number = 0;
+while(clock() - t < 1000) {
+	i = i + 1;
+}
+print(i);";
+
+fn main() {
+	let code = "fn square(a: number) -> number { a * a }";
+
+	let mut parser = Parser::new(code);
+	let parsed = parser.parse();
+	println!("{parsed:?}");
+}
+
+/*println!("ast walk: ");
+ast_walk(false);
+println!();
+println!("===================");
+println!();
+println!("stack-based bytecode vm: ");
+stack_bytecode(false);*/
+// js and python performance with the same code:
+// 		- js: 		i = ~18.8M
+// 		- python: 	i = ~6M
+
+#[allow(clippy::tabs_in_doc_comments)]
+/// Use ast-walk to run the code
+/// From the little test:
+/// 	- unoptimized:	i = ~420k
+/// 	- optimized:	i = ~2.550M
+/// And with [`rustc_hash::FxHashMap`]:
+/// 	- unoptimized:	i = ~480k
+/// 	- optimized:	i = ~3M
+#[allow(dead_code)]
+fn ast_walk(debug: bool) {
+	// use std::collections::HashMap;
+	use crate::execute::walker::walk;
+	use rustc_hash::FxHashMap as HashMap;
+
+	let mut parser = Parser::new(CODE);
+	let parsed = parser.parse();
+
+	if debug {
+		println!("parsed: {parsed:?}");
+	}
+
+	let mut locals = HashMap::default();
+	let args = [0; 2];
+
+	if debug {
+		println!("args: {args:?}");
+	}
+
+	walk(parsed.0, &mut locals, args);
+	if debug {
+		println!("locals: {locals:?} ");
+	}
+}
+
+#[allow(clippy::tabs_in_doc_comments)]
+/// Use a bytecode stack-based VM to run the code
+/// From the little test:
+/// 	- unoptimized: 	i = ~1.6M
+/// 	- optimized: 	i = ~10.5M
+#[allow(dead_code)]
+fn stack_bytecode(debug: bool) {
+	use crate::execute::stack_bytecode::{compiler::compile, vm::Vm};
+
+	let mut parser = Parser::new(CODE);
+	let parsed = parser.parse();
+
+	let compiled = compile(parsed.0.clone());
+
+	if debug {
+		println!("parsed: {parsed:?}");
+		println!("(compiled, constants): {compiled:?}");
+	}
+
+	let mut vm = Vm::new(compiled.0, compiled.1);
+
+	vm.run();
+}
